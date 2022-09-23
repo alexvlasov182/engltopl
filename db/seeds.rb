71d2b41ac41a261fcdaf2c124b29d82f412acfe9
Alex Vlasov <drumlife182@gmail.com>
2022-09-23 18:58:04 +0200

    Prepare seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,22 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user_1 = User.create(email: 'user_1@test.com', password: 'password', password_confirmation: 'password')
+user_2 = User.create(email: 'user_2@test.com', password: 'password', password_confirmation: 'password')
+
+language_1 = Language.create(name: 'English')
+language_2 = Language.create(name: 'Ukrainian')
+language_3 = Language.create(name: 'Polish')
+language_4 = Language.create(name: 'German')
+
+5.times do
+  Word.create(content: Faker::Lorem.word, language: language_1, user: user_1)
+  Word.create(content: Faker::Lorem.word, language: language_2, user: user_1)
+end
+
+5.times do
+  Word.create(content: Faker::Lorem.word, language: language_2, user: user_2)
+  Word.create(content: Faker::Lorem.word, language: language_3, user: user_2)
+end
+
+5.times do
+  Word.create(content: Faker::Lorem.word, language: language_3, user: user_1)
+  Word.create(content: Faker::Lorem.word, language: language_4, user: user_2)
+end
