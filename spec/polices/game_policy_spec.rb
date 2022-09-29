fb2ad5a11a4ecb988d5adb8a86809200cb6fbbbc
Alex Vlasov <drumlife182@gmail.com>
2022-09-29 18:08:36 +0200

    Authorize game in show action

---
+++ spec/polices/game_policy_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe GamePolicy, type: :policy do
+  subject { described_class }
+
+  permissions :show? do
+    context 'when game does not belong to user' do
+      let(:game) { build_stubbed(:game) }
+      let(:user) { build_stubbed(:user) }
+
+      it 'denies access' do
+        expect(subject).not_to permit(user, game)
+      end
+    end
+
+    context 'when game belongs to user' do
+      let(:game) { build_stubbed(:game, user: user) }
+      let(:user) { build_stubbed(:user) }
+
+      it 'grants access' do
+        expect(subject).to permit(user, game)
+      end
+    end
+  end
+end
