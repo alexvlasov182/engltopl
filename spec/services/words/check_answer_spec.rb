c5cc5599d23c2289c85188a486d71f4c50daf463
Alex Vlasov <drumlife182@gmail.com>
2022-10-03 21:53:53 +0200

    Increment game stats

--- spec/services/words/check_answer_spec.rb
+++ spec/services/words/check_answer_spec.rb
@@ -4,20 +4,38 @@ require 'rails_helper'
 
 describe Words::CheckAnswer do
   describe '#call' do
-    subject { described_class.new(word, answer).call }
+    subject { described_class.new(word, game, answer).call }
+
+    let(:game) { create(:game) }
 
     context 'when user provided good answer' do
       let(:word) { create(:word, :with_translations) }
       let(:answer) { word.translations.first.content }
 
-      it { is_expected.to eq(true)}
+      it { is_expected.to eq(true) }
+
+      it do
+        expect { subject }.to change{ game.reload.good_answers_count }
+      end
+
+      it do
+        expect { subject }.not_to change { game.reload.bad_answers_count }
+      end
     end
 
     context 'when user provided bad answer' do
       let(:word) { create(:word, :with_translations) }
       let(:answer) { 'something123' }
 
-      it { is_expected.to eq(false)}
+      it { is_expected.to eq(false) }
+
+      it do
+        expect { subject }.to change{ game.reload.bad_answers_count }
+      end
+
+      it do
+        expect { subject }.not_to change { game.reload.good_answers_count }
+      end
     end
   end
 end
